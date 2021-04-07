b8b58e5d7491653333d6b379dedb72d989709a22
roman <romankhiropulos@gmail.com>
2021-04-07 10:26:20 +0300

    Made CandidateServlet

--- src/main/java/ru/job4j/dream/model/Candidate.java
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -1,10 +1,13 @@
 package ru.job4j.dream.model;
 
+import java.util.Calendar;
 import java.util.Objects;
 
 public class Candidate {
     private int id;
     private String name;
+    private String description;
+    private Calendar created;
 
     public Candidate(int id, String name) {
         this.id = id;
