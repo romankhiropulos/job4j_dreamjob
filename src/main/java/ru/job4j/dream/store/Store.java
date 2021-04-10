73be1a58fc9d34f3fd1840db135a493b1997877a
roman <romankhiropulos@gmail.com>
2021-04-10 13:52:10 +0300

    add AtomicInteger CANDIDATE_ID

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -14,6 +14,8 @@ public class Store {
 
     private static final AtomicInteger POST_ID = new AtomicInteger(4);
 
+    private static final AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
@@ -36,7 +38,7 @@ public class Store {
 
     public void save(Candidate candidate) {
         if (candidate.getId() == 0) {
-            candidate.setId(POST_ID.incrementAndGet());
+            candidate.setId(CANDIDATE_ID.incrementAndGet());
         }
         candidates.put(candidate.getId(), candidate);
     }
