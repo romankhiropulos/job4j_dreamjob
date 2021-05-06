b80bcea1b527302018f4d5aa2be3714a4ef98662
roman <romankhiropulos@gmail.com>
2021-05-06 23:09:22 +0300

    Added UserServlet and UserServletTest

---
+++ src/test/java/ru/job4j/dream/servlet/UserServletTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.dream.servlet;
+
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(PowerMockRunner.class)
+//@PrepareForTest(ValidateService.class)
+public class UserServletTest {
+
+
+
+}
